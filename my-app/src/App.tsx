b949e8c2780c2cb867ab6e1e2a11fbbf35e6b249
CalebSmith164 <140551154+CalebSmith164@users.noreply.github.com>
2024-04-13 05:32:47 -0500

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -83,6 +83,7 @@ const ReadAlong: React.FC<ReadAlongProps> = ({ text, baseInterval = 1000 }) => {
 };
 
 function App() {
+    let texts: string = "Welcome to the treadmill! Directly in front of you, you will find the safety clip. Please clip in onto your shirt now for your own safety. To the left of the clip, you will find the start button. To the right of the clip, you will find the stop button. Please find the metal grasps on the handles to your left and right. These will measure your heart rate. In front of the left heart rate sensor are the buttons for increasing and decreasing the incline. In front of the right heart rate sensor are the buttons for increasing and decreasing the speed. Please alert a member of staff if you have any issues. Have a good workout!";
   useEffect(()=>{
     console.log("Hi");
     var msg = new SpeechSynthesisUtterance();
@@ -92,14 +93,14 @@ function App() {
     window.speechSynthesis.cancel();
     window.speechSynthesis.speak(msg);
   }, [])
+
   return (
     <div className="App">
       <div className="App-header">
         EZ Repz
       </div>
       <div className="App-body">
-        This is all of our body information.
-        Doesn't it look so cool?
+        <ReadAlong text={texts} baseInterval={450}/>
       </div>
     </div>
   );
